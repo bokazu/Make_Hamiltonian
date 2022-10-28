0a3a03f59281315ac1aa0a6454c82593c5b8d3ba
bokazu <kazu.u.d.l@outlook.com>
2022-10-28 16:39:36 +0900

    add Hamiltonian.cpp

--- Hamiltonian/Hamiltonian.cpp
+++ Hamiltonian/Hamiltonian.cpp
@@ -10,17 +10,15 @@
 using namespace std;
 
 //コピーコンストラクタ
-DNS_Hamiltonian::DNS_Hamiltonian(const DNS_Hamiltonian& h) : jset_filename(h.jset_filename),tot_site_num(h.tot_site_num),mat_dim(h.mat_dim)
+DNS_Hamiltonian::DNS_Hamiltonian(const DNS_Hamiltonian& h) : jset_filename(h.jset_filename), tot_site_num(h.tot_site_num), mat_dim(h.mat_dim)
 {
-    H = new double[mat_dim*mat_dim];
+    H = new double[mat_dim * mat_dim];
     cblas_dcopy(mat_dim * mat_dim, h.H, 1, H, 1);
     std::cout << "copy has done.\n";
 }
 
 //代入演算子
-DNS_Hamiltonian& DNS_Hamiltonian::operator=(const DNS_Hamiltonian& h){
-    cblas_dcopy(mat_dim * mat_dim, h.H, 1, H, 1);
-}
+DNS_Hamiltonian& DNS_Hamiltonian::operator=(const DNS_Hamiltonian& h) { cblas_dcopy(mat_dim * mat_dim, h.H, 1, H, 1); }
 
 //行列の初期化
 void DNS_Hamiltonian::init()
@@ -36,12 +34,3 @@ void DNS_Hamiltonian::print(int i, int j) const
 }
 //スピン演算子と状態ベクトルの計算を行う
 void spin_operator(int m, int site_i, DNS_Hamiltonian H) {}
-
-// Hamiltonianの行列要素を計算する
-//  void DNS_Hamiltonian::hamiltonian(){
-//      for(int m = 0; m < mat_dim; m++){
-//          for(int site_i = 0; site_i < tot_site_num;site_i++){
-
-//         }
-//     }
-// }
